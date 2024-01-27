4c8af3c01c9dc0d88e6d2f2327f060d9511a4a8a
Maia Araújo <jessicamaiaaraujo@gmail.com>
2024-01-27 13:55:11 -0300

    flushOperation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
 
-const screen = document.querySelector('.screen');
+const screen = document.querySelector ('.screen');
 
 function buttonClick(value){
     if(isNaN(value)){
@@ -10,7 +10,7 @@ function buttonClick(value){
     }else{
         handleNumber(value);
     }
-    screen.innerText
+    screen.innerText = buffer;
 }
 
 function handleSymbol(symbol){
@@ -32,9 +32,58 @@ function handleSymbol(symbol){
             if(buffer.length === 1){
                 buffer = '0';
             }else{
-                buffer = buffer.toString(0, buffer.length - 1);
+                buffer = buffer.substring(0, buffer.length - 1);
             }
             break;
-        case '+'        
+        case '+':
+        case '−':
+        case '×':
+        case '÷':  
+         handleMath(symbol);
+         break;
+   }
+}
+
+function handleMath(symbol){
+    if(buffer === '0'){
+        return;
+    }
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    }else{
+        flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer){
+    if(previousOperator === '+'){
+        runningTotal += intBuffer;
+    }else if(previousOperator === '−'){
+        runningTotal -= intBuffer;
+    }else if(previousOperator === '×'){
+        runningTotal *= intBuffer;
+    }else if(previousOperator === '÷'){
+        runningTotal /= intBuffer;
+ }
+}
+
+function handleNumber(numberString){
+    if(buffer === "0"){
+        buffer = numberString;
+    }else{
+        buffer += numberString
     }
 }
+
+function init(){
+    document.querySelector('.calc-buttons').
+    addEventListener('click', function(event){
+        buttonClick(event.target.innerText);
+    })
+}
+
+init();
